57312208a071120efa6f90759f25fece15f356d8
Victor Manzar <vmanzar@accendosoft.com>
2018-07-19 18:11:27 +0300

    Added global error handling

--- src/main/java/com/security/app/controller/advice/EntityExceptionHandler.java
+++ src/main/java/com/security/app/controller/advice/EntityExceptionHandler.java
@@ -1,23 +1,20 @@
 package com.security.app.controller.advice;
 
-import com.security.app.exception.BadRequestException;
-import com.security.app.exception.PermisionException;
-import com.security.app.exception.UserNotFoundException;
+import com.security.app.exception.GlobalException;
 import com.security.app.responce.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@ResponseBody
 public class EntityExceptionHandler{
 
     private static final Logger logger = LoggerFactory.getLogger(EntityExceptionHandler.class);
 
-    @ExceptionHandler(UserNotFoundException.class)
+   /* @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
     public ApiResponse userNotFound(UserNotFoundException ex){
@@ -39,5 +36,12 @@ public class EntityExceptionHandler{
     protected ApiResponse permision(PermisionException ex) {
         logger.info(ex.getMessage());
         return new ApiResponse(ex.getResponseCode(), ex.getMessage());
+    }*/
+
+    @ExceptionHandler(GlobalException.class)
+    public ApiResponse globalHandler(GlobalException exception){
+        return new ApiResponse(exception.getResponseCode(), exception.getMessage());
     }
+
+
 }
